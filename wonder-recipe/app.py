4f1f902854d3946077a00913f936caa7fdcb203f
Lawrence Dovin <lawrencedovin@gmail.com>
2021-03-09 19:49:22 -0500

    Added base app route and html file

---
+++ wonder-recipe/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import session, make_response
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
+app.debug = False
+toolbar = DebugToolbarExtension(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
