30276cdee1950197818a5ec70364fd8d0b80d90c
Lawrence Dovin <lawrencedovin@gmail.com>
2021-03-11 13:32:13 -0500

    Updated hero gradient for responsiveness

--- wonder-recipe/static/app.js
+++ wonder-recipe/static/app.js
@@ -2,6 +2,6 @@ const logo = 'Wonder Recipe';
 for(let i = 0; i < logo.length; i++) {
     let newSpan = $("<span />")
                         .text(logo[i])
-                        .css("color", i % 2 == 0 ? "#009B49" : "#8CDC09");
+                        .css("color", i % 2 == 0 && logo[i] != ' ' ? "#009B49" : "#8CDC09");
     $("#logo").append(newSpan);
 }
