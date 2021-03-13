793e2379ea753569467bc5bd58cf8004327fec44
Lawrence Dovin <lawrencedovin@gmail.com>
2021-03-12 19:02:34 -0500

    Added user lists title

--- wonder-recipe/static/app.js
+++ wonder-recipe/static/app.js
@@ -10,3 +10,18 @@ for(let i = 0; i < logo.length; i++) {
     }
     $("#logo").append('<span> </span>');
 }
+
+const userListsTitleUnderline = document.querySelector('#userListsTitleUnderline');
+
+const colors = ["#089F46", "#32B232", "#1CA83C", "#3AB630", "#4FB23", "#8CDC09", "#94E80D", "#A5FF13",
+                "#A5FF13", "#94E80D", "#8CDC09", "#4FB23", "#3AB630", "#1CA83C", "#32B232", "#089F46"];
+
+function changeColor(colors, i) {
+    setInterval(() => {
+      let linearGradient = `linear-gradient(90deg, ${colors[i]} 0%, #089F46 50%, ${colors[i]} 100%)`;
+      userListsTitleUnderline.style.background = linearGradient;
+      i++;
+      i %= colors.length;
+    }, 250);
+  }
+  changeColor(colors, 0);
