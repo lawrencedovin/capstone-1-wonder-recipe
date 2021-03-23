df75240660957490ff0de3f22cc8a89ac458ec0f
Lawrence Dovin <lawrencedovin@gmail.com>
2021-03-23 16:58:59 -0400

    Added initial json format for food from API call

--- wonder-recipe/wonderfoods.py
+++ wonder-recipe/wonderfoods.py
@@ -10,12 +10,22 @@ class WonderFood:
     def serialize(self):
         payload = {'apiKey': self.apiKey, 'cuisine': self.cuisine, 'number': self.number}
         response_wonder_food = requests.get('https://api.spoonacular.com/recipes/complexSearch/', params=payload)
-        print(response_wonder_food.text)
+        # print(response_wonder_food.text)
 
-        return {
-            "data": response_wonder_food.text,
-            "cuisine": self.cuisine
-        }
+        json_response = response_wonder_food.json()
+
+        food_dictionary = {}
+        food_list = []
+
+        for response in json_response["results"]:
+            food_dictionary["id"] = response["id"]
+            food_dictionary["title"] = response["title"]
+            food_dictionary["image"] = response["image"]
+            
+            food_dictionary_copy = food_dictionary.copy()
+            food_list.append(food_dictionary_copy)
+
+        return food_list
 
     def __repr__(self):
         return f'<Wonder Food {self.apiKey} cuisine={self.cuisine} number={self.number}>'
