0eb21e7608fc3a7c63a2eb2304d7e60b800b142e
Lawrence Dovin <lawrencedovin@gmail.com>
2021-04-01 17:30:13 -0400

    Refactored seeds file

--- wonder-recipe/seeds.py
+++ wonder-recipe/seeds.py
@@ -11,10 +11,12 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.sql import text
 
 # Create all tables
+####################################
 db.drop_all()
 db.create_all()
 
 # If table isn't empty, empty it
+####################################
 User.query.delete()
 Cuisine.query.delete()
 Diet.query.delete()
@@ -24,23 +26,27 @@ GroceryList.query.delete()
 Likes.query.delete()
 
 # Add users
+####################################
 lawrence = User(username='lawrence123', email='lawrence@gmail.com', password='abc123', phone_number='6105791888')
 julie = User(username='julie1', email='julie@gmail.com', password='123abc', phone_number='6105791885')
 miguel = User(username='miguel91', email='miguel@gmail.com', password='00123', phone_number='6105791845')
 
 # Add cuisine
+####################################
 cuisine_list = []
 for cuisine in cuisines:
    add_cuisine = Cuisine(title=cuisine)
    cuisine_list.append(add_cuisine)
 
 # Add diet
+####################################
 diet_list = []
 for diet in diets:
    add_diet = Diet(title=diet)
    diet_list.append(add_diet)
 
 # Adds new User, Cuisine, Diet objects
+####################################
 db.session.add_all([lawrence, julie, miguel])
 db.session.add_all(cuisine_list)
 db.session.add_all(diet_list)
@@ -71,44 +77,9 @@ for cuisine in cuisines:
           db.session.commit()
       except exc.IntegrityError:
           db.session.rollback()
-      
-# try:
-#     db.session.add_all(recipe_list)
-#     db.session.commit()
-# except exc.IntegrityError:
-#     db.session.rollback()      
-    #   recipe_list.append(recipe)
-
-# db.session.execute(insert(text('recipes'))
-#                 .values(recipe_list)
-#                 .on_conflict_do_nothing())
-# db.session.add_all(recipe_list).prefix_with("OR REPLACE")
-
-# try:
-#     db.session.add_all(recipe_list)
-#     db.session.commit()
-# except exc.IntegrityError:
-#     db.session.rollback()
 
 # Adds Recipe and Diet for M:M relationship
 ####################################
-# recipe_diet_list = []
-# recipes = Recipe.query.all()
-# diets = Diet.query.all()
-
-# for recipe_diet in recipes:
-
-
-
-# CUISINE_URL = 'https://api.spoonacular.com/recipes/complexSearch/'
-# cuisine_payload = {'apiKey': API_KEY, 'cuisine': 'african', 'number': 1, 'addRecipeInformation': True}
-# response_cuisine = requests.get(CUISINE_URL, params=cuisine_payload)
-
-# cuisine_json_response = response_cuisine.json()
-
-# for response in cuisine_json_response["results"]:
-#    for diet in response["diets"]:
-#       print(diet)
 
 for cuisine in cuisines:
    recipes = WonderRecipe(apiKey=API_KEY, cuisine=cuisine, number=1)
@@ -132,16 +103,6 @@ for cuisine in cuisines:
             db.session.commit()
         except exc.IntegrityError:
             db.session.rollback()
-
-# recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
-#     diet_id = db.Column(db.Integer, db.ForeignKey('diets.id'), primary_key=True)
-
-
-   # diets = response["diets"][0]
-# pancake_vegan = RecipeDiet(recipe_id=1234, diet_id=1)
-# pancake_vegetarian = RecipeDiet(recipe_id=1234, diet_id=2)
-
-# recipe_diet_list = []
 
 # Add Grocery List
 ####################################
