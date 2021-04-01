e0423ab07ed9e114226c4584a9a955d83dfcb447
Lawrence Dovin <lawrencedovin@gmail.com>
2021-04-01 18:19:37 -0400

    Updated db queries file

--- wonder-recipe/dbqueries.sql
+++ wonder-recipe/dbqueries.sql
@@ -1,30 +1,30 @@
-SELECT r.title, c.title
+SELECT r.title as recipe, c.title as cuisine
     FROM recipes r
     JOIN cuisines c
         ON r.cuisine_id = c.id;
 
-SELECT r.title, d.title
+SELECT r.title as recipe, d.title as diet
     FROM recipe_diet rd
     JOIN recipes r
         ON r.id = rd.recipe_id
     JOIN diets d
         ON d.id = rd.diet_id;
 
-SELECT r.title, c.title
+SELECT r.title as recipe, c.title as cuisine
     FROM recipe_cuisine rc
     JOIN recipes r
         ON r.id = rc.recipe_id
     JOIN cuisines c
         ON c.id = rc.cuisine_id;
 
-SELECT u.username, r.recipe_name
+SELECT u.username as user, r.recipe_name as recipe
     FROM grocery_list gl
     JOIN users u
         ON u.id = gl.user_id
     JOIN recipes r
         ON r.id = gl.recipe_id;
 
-SELECT u.username, r.recipe_name
+SELECT u.username as user, r.recipe_name as recipe
     FROM likes lr
     JOIN users u
         ON u.id = lr.user_id
